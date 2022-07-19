facb902eda32fbdc60341ea9ed4db7fea56bf4d8
RubensKanto <rpbkanto@gmail.com>
2022-07-19 11:29:50 -0300

    alterando arquivo principal

--- src/br/impacta/Principal.java
+++ src/br/impacta/Principal.java
@@ -5,7 +5,7 @@ import br.impacta.controller.ControleDeCadastro;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		//alterando arquivo
 			
 			ControleDeCadastro controleCadastro = new ControleDeCadastro();
 			controleCadastro.iniciarPrograma();
